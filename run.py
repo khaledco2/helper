6bb5ae9d28400b55fc9a880e1cc58d00deb3c4f9
khaledco2 <115948625+khaledco2@users.noreply.github.com>
2026-02-12 05:59:42 +0200

    Update run.py

--- run.py
+++ run.py
@@ -8,8 +8,8 @@ import ctypes
 from PIL import ImageGrab
 
 # --- إعدادات القوة والاستجابة ---
-SENSITIVITY = 1.5   # رفعنا الحساسية لضمان "التمسك" بالهدف
-ACCURACY = 0.1     # عتبة الحركة (كلما قل زاد التحسس)
+SENSITIVITY = 0.5   # رفعنا الحساسية لضمان "التمسك" بالهدف
+ACCURACY = 5    # عتبة الحركة (كلما قل زاد التحسس)
 is_running = False
 
 def start_engine():
